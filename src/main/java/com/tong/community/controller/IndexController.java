bf2045d833aa5a5149aa5bace4f1958874a501aa
Treowe <trorawe@163.com>
2019-12-29 22:53:10 +0800

    add check cookies

--- src/main/java/com/tong/community/controller/IndexController.java
+++ src/main/java/com/tong/community/controller/IndexController.java
@@ -1,10 +1,16 @@
 package com.tong.community.controller;
 
+import com.tong.community.mapper.UserMapper;
+import com.tong.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author: TONG
@@ -14,8 +20,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/")
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String hello() {
+    public String index(HttpServletRequest request) {
+        Cookie [] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 }
