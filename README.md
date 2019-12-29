8a90f8ade931b83830a1d0db42b43b69407cf709
Treowe <trorawe@163.com>
2019-12-29 22:18:26 +0800

    新增github登录并保存登录数据信息

--- README.md
+++ README.md
@@ -8,3 +8,15 @@
 ## 工具
 [Visual Paradigm](http://www.visual-paradigm.com)
 
+## SQL脚本
+```sql
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` varchar(100) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `token` char(36) DEFAULT NULL,
+  `gmt_create` bigint(20) DEFAULT NULL,
+  `gmt_modified` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+```
--- src/main/java/com/tong/community/controller/AuthorizeController.java
+++ src/main/java/com/tong/community/controller/AuthorizeController.java
@@ -2,12 +2,17 @@ package com.tong.community.controller;
 
 import com.tong.community.dto.AccessTokenDTO;
 import com.tong.community.dto.GithubUser;
+import com.tong.community.mapper.UserMapper;
+import com.tong.community.model.User;
 import com.tong.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 /**
  * @author: TONG
@@ -21,14 +26,20 @@ public class AuthorizeController {
 
     @Value("${github.client.id}")
     private String clientId;
+
     @Value("${github.client.secret}")
     private String clientSecret;
+
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+    
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state) {
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -36,8 +47,22 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null) {
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            // 登录成功，写cookie 和 session
+            request.getSession().setAttribute("user", githubUser);
+            return "redirect:/";
+
+        } else {
+            // 登录失败，重新登录
+            return "redirect:/";
+        }
     }
 }
